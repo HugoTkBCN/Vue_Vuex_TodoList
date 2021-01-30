2442bd8f78a06240d5f6c7db3e2f9d325e821a7d
Hugo <hugolachkar@gmail.com>
2021-01-30 12:05:23 +0100

    [ADD] Header.vue -> add header styled + task counter

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <header>
+    <h1>My Todo's ({{ nbrTasks.length }})</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  computed: {
+    nbrTasks() {
+      return this.$store.getters.getTasks;
+    }
+  }
+};
+</script>
+
+<style>
+</style>
