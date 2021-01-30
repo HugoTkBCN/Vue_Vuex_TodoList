3c731a7d00019209a2656324d2e0ecb476c2c790
Hugo <hugolachkar@gmail.com>
2021-01-30 11:57:26 +0100

    [UPDATE] App.js -> set page structure + set style

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<script>
+import Header from "@/components/Header.vue";
+import TodoNew from "@/components/Todo/TodoNew.vue";
+import TodoList from "@/components/Todo/TodoList.vue";
+
+export default {
+  components: {
+    Header,
+    TodoNew,
+    TodoList,
+  },
+};
+</script>
+
+<template>
+  <div class="container">
+    <div id="app">
+      <div>
+        <Header />
+        <TodoList />
+        <TodoNew />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+html {
+  background: linear-gradient(45deg, #c15151 25%, #b89c41);
+  height: 100%;
+}
+
+body {
+  display: flex;
+  height: 100%;
+  margin: 0;
+}
+
+.container {
+  width: 30%;
+  margin: auto;
+  background-color: white;
+  border-radius: 15px;
+  padding: 1%;
+  box-shadow: 0 0 1rem rgba(0, 0, 0, 0.5);
+}
+
+h1 {
+  text-align: center;
+}
+
+</style>
